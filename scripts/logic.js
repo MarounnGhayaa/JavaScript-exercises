94985feadf951c5e21529a319f0cfaf096f25c65
MarounnGhayaa <marounghaya@icloud.com>
2025-04-13 20:24:14 +0300

    Creating a function to generate reports for students

---
+++ scripts/logic.js
@@ -0,0 +1,27 @@
+const students = [
+    { name: "Alice", scores: [90, 85, 92] },
+    { name: "Bob", scores: [70, 68, 72] },
+    { name: "Charlie", scores: [100, 100, 100] }
+  ];
+function generateReports(students){
+    for (const student of students) {
+        const avg = (student.scores[0] + student.scores[1] + student.scores[2])/3;
+        if (avg >= 90) {
+            grade = "A"
+        }
+        else if (avg >= 80) {
+            grade = "B"
+        }
+        else if (avg >= 70) {
+            grade = "C"
+        }
+        else if (avg >= 60) {
+            grade = "D"
+        }
+        else {
+            grade = "F"
+        }
+        console.log( "name: " + student.name + " average: " + avg + " grade: " + grade)
+    }
+}
+generateReports(students)
