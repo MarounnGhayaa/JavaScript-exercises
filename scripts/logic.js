dda0626ef5cc5cc87528a974e9df8225f0c91f0c
MarounnGhayaa <marounghaya@icloud.com>
2025-04-14 23:08:57 +0300

    Creating a print history method

--- scripts/logic.js
+++ scripts/logic.js
@@ -3,7 +3,9 @@ const students = [
     { name: "Bob", scores: [70, 68, 72] },
     { name: "Charlie", scores: [100, 100, 100] }
   ];
+//O(1) * O(n) = O(n)
 function generateReports(students){
+    //O*(1)
     const reports = []
     //O(n) where n is the number of students
     for (const student of students) {
@@ -43,15 +45,18 @@ class BankAccount {
     constructor (ownerName, initialBalance) {
         this.ownerName = ownerName;
         this.initialBalance = initialBalance;
+        this.history = [];
     }
     //O(1)
     deposit(amount){
         this.initialBalance += amount;
+        this.history.push("Deposited $" + amount)
     }
     //O(1)
     withdraw(amount){
         if (this.initialBalance >= amount) {
             this.initialBalance -= amount
+            this.history.push("Withdrew $" + amount);
         }
         else {
             console.log("Insufficient balance")
@@ -62,6 +67,7 @@ class BankAccount {
         if (this.initialBalance >= amount) {
             this.initialBalance -= amount
             anotherAccount.deposit(amount)
+            this.history.push("Transferred $"+ amount + " to " + anotherAccount.ownerName);
         }
         else {
             console.log("Insufficient balance")
@@ -70,7 +76,17 @@ class BankAccount {
     //O(1)
     getSummary() {
         return this.ownerName + "'s balance is $" + this.initialBalance;
-    }    
+    }
+
+    //O(1) * O(n) = O(n)
+    printHistory() {  
+        //O(1)        
+        console.log(this.ownerName + "'s History: ");
+        //O(n) where n is the number of strings added to the array "history"
+        for (const transaction of this.history) { 
+            console.log(transaction);
+        }
+    }
 }
 
 const acc1 = new BankAccount("John", 500);
@@ -78,3 +94,4 @@ const acc2 = new BankAccount("Sara", 300);
 acc1.transferTo(acc2, 200); 
 console.log(acc1.getSummary()); // John's balance is $300, O(1)
 console.log(acc2.getSummary()); // Sara's balance is $500, O(1) 
+acc1.printHistory();
